31076d696fe73d047b5bb1d35c1634375bcafed3
plumbersgetpaid <plumbersgetpaid@gmail.com>
2026-08-08 12:14:36 +0000

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,8 @@ import { supabaseAdmin } from "./lib/supabaseClient";
 import Link from "next/link";
 
 export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+export const revalidate = 0;
 
 export default async function Dashboard() {
   const db = supabaseAdmin();
