2ce93d245651e29f0aba45af227fe7c31ddc5ed4
plumbersgetpaid <plumbersgetpaid@gmail.com>
2026-08-10 15:08:39 +0000

    Add files via upload

--- app/jobs/new/page.jsx
+++ app/jobs/new/page.jsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import BackButton from "../components/BackButton";
 import { getBusinessSettings } from "../../lib/getBusinessSettings";
 import VoiceQuoteAssist from "./VoiceQuoteAssist";
 
@@ -7,9 +8,7 @@ export default async function NewQuote() {
   return (
     <main>
       <div style={{ display: "flex", alignItems: "center", gap: 10 }}>
-        <Link href="/" aria-label="Back" style={backButtonStyle}>
-          ←
-        </Link>
+        <BackButton fallbackHref="/" />
         <h1 style={{ fontSize: 20, margin: 0 }}>New quote</h1>
       </div>
 
