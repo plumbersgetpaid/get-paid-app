da94a95df109a11dc238fa3418632b35ecd1f61c
plumbersgetpaid <plumbersgetpaid@gmail.com>
2026-08-13 19:19:09 +0000

    Update page.jsx

--- app/jobs/photos/[jobId]/page.jsx
+++ app/jobs/photos/[jobId]/page.jsx
@@ -4,6 +4,8 @@ import BackButton from "../../../components/BackButton";
 import Link from "next/link";
 
 export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+export const revalidate = 0;
 
 export default async function JobPhotos({ params, searchParams }) {
   const { jobId } = params;
