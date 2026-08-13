90f72186fda27613319a911b1e85b23761407d67
plumbersgetpaid <plumbersgetpaid@gmail.com>
2026-08-13 19:16:01 +0000

    Update page.jsx

--- app/calendar/reminder/new/page.jsx
+++ app/calendar/reminder/new/page.jsx
@@ -4,6 +4,8 @@ import { getBusinessSettings } from "../../../lib/getBusinessSettings";
 import VoiceReminderAssist from "./VoiceReminderAssist";
 
 export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+export const revalidate = 0;
 
 export default async function NewReminder() {
   const settings = await getBusinessSettings();
