56fe5903c8a6d165884d2985255979b376a921ed
plumbersgetpaid <plumbersgetpaid@gmail.com>
2026-08-14 16:35:49 +0000

    Update page.jsx

--- app/account/page.jsx
+++ app/account/page.jsx
@@ -22,13 +22,11 @@ export default async function Account() {
       </div>
 
       <section style={cardStyle}>
-        <div style={{ fontSize: 13, color: "#888" }}>Email</div>
-        <div style={{ fontSize: 15, marginBottom: 4 }}>{currentMember.email}</div>
-        <div style={{ fontSize: 13, color: "#888", marginTop: 10 }}>Role</div>
+        <div style={{ fontSize: 13, color: "#888" }}>Role</div>
         <div style={{ fontSize: 15, textTransform: "capitalize" }}>{currentMember.role}</div>
       </section>
 
-      <AccountForm currentName={currentMember.name} />
+      <AccountForm currentName={currentMember.name} currentEmail={currentMember.email} />
 
       <div style={{ marginTop: 24 }}>
         <LogoutButton />
