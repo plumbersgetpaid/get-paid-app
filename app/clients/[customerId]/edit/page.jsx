2d6de7bc3e3043932cdaa2ff50253b0d0075f554
plumbersgetpaid <plumbersgetpaid@gmail.com>
2026-08-15 11:17:27 +0000

    Update page.jsx

--- app/clients/[customerId]/edit/page.jsx
+++ app/clients/[customerId]/edit/page.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import { notFound } from "next/navigation";
 import { supabaseAdmin } from "../../../lib/supabaseClient";
 import { getCurrentTeamMember } from "../../../lib/auth";
-import { canSeeEverything } from "../../../lib/permissions";
+import { canSeeClientDatabase } from "../../../lib/permissions";
 import BackButton from "../../../components/BackButton";
 
 export const dynamic = "force-dynamic";
@@ -11,7 +11,7 @@ export const revalidate = 0;
 
 export default async function EditClient({ params }) {
   const currentMember = await getCurrentTeamMember();
-  if (!canSeeEverything(currentMember)) {
+  if (!canSeeClientDatabase(currentMember)) {
     notFound();
   }
 
@@ -75,20 +75,6 @@ const inputStyle = {
   borderRadius: 8,
   border: "1px solid #ddd",
   fontSize: 15,
-};
-
-const backButtonStyle = {
-  background: "white",
-  border: "1px solid #ddd",
-  borderRadius: 8,
-  width: 36,
-  height: 36,
-  fontSize: 18,
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-  textDecoration: "none",
-  color: "#111",
 };
 
 const cancelButtonStyle = {
