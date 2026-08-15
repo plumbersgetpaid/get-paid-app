67c794bd7b22064240c51d6dc486052a92386803
plumbersgetpaid <plumbersgetpaid@gmail.com>
2026-08-15 18:21:19 +0000

    Update route.js

--- app/api/team/delete/route.js
+++ app/api/team/delete/route.js
@@ -1,6 +1,6 @@
-import { supabaseAdmin } from "../../../lib/supabaseClient";
 import { getCurrentTeamMember } from "../../../lib/auth";
 import { canSeeEverything } from "../../../lib/permissions";
+import { getScopedDb } from "../../../lib/scopedSupabaseClient";
 import { NextResponse } from "next/server";
 
 export async function POST(req) {
@@ -20,7 +20,7 @@ export async function POST(req) {
     return NextResponse.json({ error: "You can't delete your own account" }, { status: 400 });
   }
 
-  const db = supabaseAdmin();
+  const db = await getScopedDb(currentMember);
 
   const { data: target } = await db
     .from("team_members")
