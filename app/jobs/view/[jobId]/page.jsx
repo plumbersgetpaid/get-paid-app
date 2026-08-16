6071d30378edbf657a9e789e4a181c7e6d24d127
plumbersgetpaid <plumbersgetpaid@gmail.com>
2026-08-16 12:37:50 +0000

    Update page.jsx

--- app/jobs/view/[jobId]/page.jsx
+++ app/jobs/view/[jobId]/page.jsx
@@ -8,6 +8,7 @@ import { getScopedDb } from "../../../lib/scopedSupabaseClient";
 import BackButton from "../../../components/BackButton";
 import AssignAndShareControl from "../../../components/AssignAndShareControl";
 import DeleteJobButton from "../../../components/DeleteJobButton";
+import ReloadOnBack from "../../../components/ReloadOnBack";
 
 export const dynamic = "force-dynamic";
 export const fetchCache = "force-no-store";
@@ -112,6 +113,7 @@ export default async function ViewJob({ params }) {
 
   return (
     <main>
+            <ReloadOnBack />
       <div style={{ display: "flex", alignItems: "center", gap: 10 }}>
         <BackButton fallbackHref="/work?tab=jobs" />
         <h1 style={{ fontSize: 20, margin: 0 }}>Job details</h1>
