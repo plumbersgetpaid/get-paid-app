dd8591083f8e077c3383b05af9d2a0651f405ee0
plumbersgetpaid <plumbersgetpaid@gmail.com>
2026-08-17 08:26:24 +0000

    Update page.jsx

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -8,7 +8,7 @@ export default function Login() {
   return (
     <main style={{ maxWidth: 400, margin: "60px auto", padding: "0 20px" }}>
       <div style={{ textAlign: "center", marginBottom: 24 }}>
-        <img src="/logo-black.svg" alt="Patch Up" width={160} style={{ maxWidth: "60%" }} />
+        <img src="/04-logo-on-white-1254.png" alt="Patch Up" width={160} style={{ maxWidth: "60%" }} />
       </div>
       <LoginForm />
     </main>
